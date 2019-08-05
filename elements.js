ca5fd3ceda73395fe988ba147a7d4e00c013df0f
vmdharan <vmdharan@users.noreply.github.com>
2019-08-05 15:21:48 +1000

    Updated to display in Electron window
    - Renamed main.css and main.js to elements.css and elements.js respectively.
    - Added a package.json and main.js to display an electron window.

---
+++ elements.js
@@ -0,0 +1,308 @@
+
+var row1 = [
+    { Number: '1', Symbol: 'H', Name: 'Hydrogen', Type: 'Non-metal' },
+    {}, {}, {}, {}, {}, {}, {}, {},
+    {}, {}, {}, {}, {}, {}, {}, {},
+    { Number: '2', Symbol: 'He', Name: 'Helium', Type: 'Noble Gas' }
+];
+
+var row2 = [
+    { Number: '3', Symbol: 'Li', Name: 'Lithium', Type: 'Alkali Metal' },
+    { Number: '4', Symbol: 'Be', Name: 'Beryllium', Type: 'Alkaline Earth' },
+    {}, {}, {}, {}, {}, {}, {}, {}, {}, {},
+    { Number: '5', Symbol: 'B', Name: 'Boron', Type: 'Metalloid' },
+    { Number: '6', Symbol: 'C', Name: 'Carbon', Type: 'Non-metal' },
+    { Number: '7', Symbol: 'N', Name: 'Nitrogen', Type: 'Non-metal' },
+    { Number: '8', Symbol: 'O', Name: 'Oxygen', Type: 'Non-metal' },
+    { Number: '9', Symbol: 'F', Name: 'Fluorine', Type: 'Halogen' },
+    { Number: '10', Symbol: 'Ne', Name: 'Neon', Type: 'Noble Gas' }
+];
+
+var row3 = [
+    { Number: '11', Symbol: 'Na', Name: 'Sodium', Type: 'Alkali Metal' },
+    { Number: '12', Symbol: 'Mg', Name: 'Magnesium', Type: 'Alkaline Earth' },
+    {}, {}, {}, {}, {}, {}, {}, {}, {}, {},
+    { Number: '13', Symbol: 'Al', Name: 'Aluminium', Type: 'Basic Metal' },
+    { Number: '14', Symbol: 'Si', Name: 'Silicon', Type: 'Metalloid' },
+    { Number: '15', Symbol: 'P', Name: 'Phosphorus', Type: 'Non-metal' },
+    { Number: '16', Symbol: 'S', Name: 'Sulphur', Type: 'Non-metal' },
+    { Number: '17', Symbol: 'Cl', Name: 'Chlorine', Type: 'Halogen' },
+    { Number: '18', Symbol: 'Ar', Name: 'Argon', Type: 'Noble Gas' }
+];
+
+var row4 = [
+    { Number: '19', Symbol: 'K', Name: 'Potassium', Type: 'Alkali Metal' },
+    { Number: '20', Symbol: 'Ca', Name: 'Calcium', Type: 'Alkaline Earth' },
+    { Number: '21', Symbol: 'Sc', Name: 'Scandium', Type: 'Transition Metal' },
+    { Number: '22', Symbol: 'Ti', Name: 'Titanium', Type: 'Transition Metal' },
+    { Number: '23', Symbol: 'V', Name: 'Vanadium', Type: 'Transition Metal' },
+    { Number: '24', Symbol: 'Cr', Name: 'Chromium', Type: 'Transition Metal' },
+    { Number: '25', Symbol: 'Mn', Name: 'Manganese', Type: 'Transition Metal' },
+    { Number: '26', Symbol: 'Fe', Name: 'Iron', Type: 'Transition Metal' },
+    { Number: '27', Symbol: 'Co', Name: 'Cobalt', Type: 'Transition Metal' },
+    { Number: '28', Symbol: 'Ni', Name: 'Nickel', Type: 'Transition Metal' },
+    { Number: '29', Symbol: 'Cu', Name: 'Copper', Type: 'Transition Metal' },
+    { Number: '30', Symbol: 'Zn', Name: 'Zinc', Type: 'Transition Metal' },
+    { Number: '31', Symbol: 'Ga', Name: 'Gallium', Type: 'Basic Metal' },
+    { Number: '32', Symbol: 'Ge', Name: 'Germanium', Type: 'Metalloid' },
+    { Number: '33', Symbol: 'As', Name: 'Arsenic', Type: 'Metalloid' },
+    { Number: '34', Symbol: 'Se', Name: 'Selenium', Type: 'Non-metal' },
+    { Number: '35', Symbol: 'Br', Name: 'Bromine', Type: 'Halogen' },
+    { Number: '36', Symbol: 'Kr', Name: 'Krypton', Type: 'Noble Gas' }
+];
+
+var row5 = [
+    { Number: '37', Symbol: 'Rb', Name: 'Rubidium', Type: 'Alkali Metal' },
+    { Number: '38', Symbol: 'Sr', Name: 'Strontium', Type: 'Alkaline Earth' },
+    { Number: '39', Symbol: 'Y', Name: 'Yttrium', Type: 'Transition Metal' },
+    { Number: '40', Symbol: 'Zr', Name: 'Zirconium', Type: 'Transition Metal' },
+    { Number: '41', Symbol: 'Nb', Name: 'Niobium', Type: 'Transition Metal' },
+    { Number: '42', Symbol: 'Mo', Name: 'Molybdenum', Type: 'Transition Metal' },
+    { Number: '43', Symbol: 'Tc', Name: 'Technetium', Type: 'Transition Metal' },
+    { Number: '44', Symbol: 'Ru', Name: 'Ruthenium', Type: 'Transition Metal' },
+    { Number: '45', Symbol: 'Rh', Name: 'Rhodium', Type: 'Transition Metal' },
+    { Number: '46', Symbol: 'Pd', Name: 'Palladium', Type: 'Transition Metal' },
+    { Number: '47', Symbol: 'Ag', Name: 'Silver', Type: 'Transition Metal' },
+    { Number: '48', Symbol: 'Cd', Name: 'Cadmium', Type: 'Transition Metal' },
+    { Number: '49', Symbol: 'In', Name: 'Indium', Type: 'Basic Metal' },
+    { Number: '50', Symbol: 'Sn', Name: 'Tin', Type: 'Basic Metal' },
+    { Number: '51', Symbol: 'Sb', Name: 'Antimony', Type: 'Metalloid' },
+    { Number: '52', Symbol: 'Te', Name: 'Tellurium', Type: 'Metalloid' },
+    { Number: '53', Symbol: 'I', Name: 'Iodine', Type: 'Halogen' },
+    { Number: '54', Symbol: 'Xe', Name: 'Xenon', Type: 'Noble Gas' }
+];
+
+var row6 = [
+    { Number: '55', Symbol: 'Cs', Name: 'Cesium', Type: 'Alkali Metal' },
+    { Number: '56', Symbol: 'Ba', Name: 'Barium', Type: 'Alkaline Earth' },
+    { Number: '57-71', Symbol: '', Name: '', Type: '' },
+    { Number: '72', Symbol: 'Hf', Name: 'Halfnium', Type: 'Transition Metal' },
+    { Number: '73', Symbol: 'Ta', Name: 'Tantalum', Type: 'Transition Metal' },
+    { Number: '74', Symbol: 'W', Name: 'Tungsten', Type: 'Transition Metal' },
+    { Number: '75', Symbol: 'Re', Name: 'Rhenium', Type: 'Transition Metal' },
+    { Number: '76', Symbol: 'Os', Name: 'Osmium', Type: 'Transition Metal' },
+    { Number: '77', Symbol: 'Ir', Name: 'Iridium', Type: 'Transition Metal' },
+    { Number: '78', Symbol: 'Pt', Name: 'Platinum', Type: 'Transition Metal' },
+    { Number: '79', Symbol: 'Au', Name: 'Gold', Type: 'Transition Metal' },
+    { Number: '80', Symbol: 'Hg', Name: 'Mercury', Type: 'Transition Metal' },
+    { Number: '81', Symbol: 'Tl', Name: 'Thallium', Type: 'Basic Metal' },
+    { Number: '82', Symbol: 'Pb', Name: 'Lead', Type: 'Basic Metal' },
+    { Number: '83', Symbol: 'Bi', Name: 'Bismuth', Type: 'Basic Metal' },
+    { Number: '84', Symbol: 'Po', Name: 'Polonium', Type: 'Metalloid' },
+    { Number: '85', Symbol: 'At', Name: 'Astatine', Type: 'Halogen' },
+    { Number: '86', Symbol: 'Rn', Name: 'Radon', Type: 'Noble Gas' }
+];
+
+var row7 = [
+    { Number: '87', Symbol: 'Fr', Name: 'Francium', Type: 'Alkali Metal' },
+    { Number: '88', Symbol: 'Ra', Name: 'Radium', Type: 'Alkaline Earth' },
+    { Number: '89-103', Symbol: '', Name: '', Type: '' },
+    { Number: '104', Symbol: 'Rf', Name: 'Rutherfordium', Type: 'Transition Metal' },
+    { Number: '105', Symbol: 'Db', Name: 'Dubnium', Type: 'Transition Metal' },
+    { Number: '106', Symbol: 'Sg', Name: 'Seaborgium', Type: 'Transition Metal' },
+    { Number: '107', Symbol: 'Bh', Name: 'Bohrium', Type: 'Transition Metal' },
+    { Number: '108', Symbol: 'Hs', Name: 'Hassium', Type: 'Transition Metal' },
+    { Number: '109', Symbol: 'Mt', Name: 'Meitnerium', Type: 'Transition Metal' },
+    { Number: '110', Symbol: 'Ds', Name: 'Darmstadtium', Type: 'Transition Metal' },
+    { Number: '111', Symbol: 'Rg', Name: 'Roentgenium', Type: 'Transition Metal' },
+    { Number: '112', Symbol: 'Cn', Name: 'Copernicium', Type: 'Transition Metal' },
+    { Number: '113', Symbol: 'Nh', Name: 'Nihonium', Type: 'Basic Metal' },
+    { Number: '114', Symbol: 'Fl', Name: 'Flerovium', Type: 'Basic Metal' },
+    { Number: '115', Symbol: 'Mc', Name: 'Moscovium', Type: 'Basic Metal' },
+    { Number: '116', Symbol: 'Lv', Name: 'Livermorium', Type: 'Basic Metal' },
+    { Number: '117', Symbol: 'Ts', Name: 'Tennessine', Type: 'Halogen' },
+    { Number: '118', Symbol: 'Og', Name: 'Oganesson', Type: 'Noble Gas' }
+];
+
+var row_Lanthanide = [
+    { Number: '57', Symbol: 'La', Name: 'Lanthanum', Type: 'Lanthanide' },
+    { Number: '58', Symbol: 'Ce', Name: 'Cerium', Type: 'Lanthanide' },
+    { Number: '59', Symbol: 'Pr', Name: 'Praseodymium', Type: 'Lanthanide' },
+    { Number: '60', Symbol: 'Nd', Name: 'Neodymium', Type: 'Lanthanide' },
+    { Number: '61', Symbol: 'Pm', Name: 'Promethium', Type: 'Lanthanide' },
+    { Number: '62', Symbol: 'Sm', Name: 'Samarium', Type: 'Lanthanide' },
+    { Number: '63', Symbol: 'Eu', Name: 'Europium', Type: 'Lanthanide' },
+    { Number: '64', Symbol: 'Gd', Name: 'Gadolinium', Type: 'Lanthanide' },
+    { Number: '65', Symbol: 'Tb', Name: 'Terbium', Type: 'Lanthanide' },
+    { Number: '66', Symbol: 'Dy', Name: 'Dysprosium', Type: 'Lanthanide' },
+    { Number: '67', Symbol: 'Ho', Name: 'Holmium', Type: 'Lanthanide' },
+    { Number: '68', Symbol: 'Er', Name: 'Erbium', Type: 'Lanthanide' },
+    { Number: '69', Symbol: 'Tm', Name: 'Thulium', Type: 'Lanthanide' },
+    { Number: '70', Symbol: 'Yb', Name: 'Ytterbium', Type: 'Lanthanide' },
+    { Number: '71', Symbol: 'Lu', Name: 'Lutetium', Type: 'Lanthanide' }
+];
+
+var row_Actinide = [
+    { Number: '89', Symbol: 'Ac', Name: 'Actinium', Type: 'Actinide' },
+    { Number: '90', Symbol: 'Th', Name: 'Thorium', Type: 'Actinide' },
+    { Number: '91', Symbol: 'Pa', Name: 'Protactinium', Type: 'Actinide' },
+    { Number: '92', Symbol: 'U', Name: 'Uranium', Type: 'Actinide' },
+    { Number: '93', Symbol: 'Np', Name: 'Neptunium', Type: 'Actinide' },
+    { Number: '94', Symbol: 'Pu', Name: 'Plutonium', Type: 'Actinide' },
+    { Number: '95', Symbol: 'Am', Name: 'Americium', Type: 'Actinide' },
+    { Number: '96', Symbol: 'Cm', Name: 'Curium', Type: 'Actinide' },
+    { Number: '97', Symbol: 'Bk', Name: 'Berkelium', Type: 'Actinide' },
+    { Number: '98', Symbol: 'Cf', Name: 'Californium', Type: 'Actinide' },
+    { Number: '99', Symbol: 'Es', Name: 'Einsteinium', Type: 'Actinide' },
+    { Number: '100', Symbol: 'Fm', Name: 'Fermium', Type: 'Actinide' },
+    { Number: '101', Symbol: 'Md', Name: 'Mendelevium', Type: 'Actinide' },
+    { Number: '102', Symbol: 'No', Name: 'Nobelium', Type: 'Actinide' },
+    { Number: '103', Symbol: 'Lr', Name: 'Lawrencium', Type: 'Actinide' }
+];
+
+
+function createCard(cardImg, elemNumber, elemSymbol, elemName) {
+    var card = document.createElement('div');
+
+    var header = document.createElement('div');
+    var img = document.createElement('div');
+    var footer = document.createElement('div');
+
+    // Create a header with the element number.
+    header.classList.add("elem-card-text-top");
+    header.textContent = elemNumber;
+
+    // Display the element symbol.
+    card.classList.add("elem-card");
+    img.classList.add("elem-img-div");
+    img.classList.add(cardImg);
+    img.textContent = elemSymbol;
+
+    // Create a footer with the element name.
+    footer.classList.add("elem-card-text-bottom");
+    footer.textContent = elemName;
+
+    card.appendChild(header);
+    card.appendChild(img);
+    card.appendChild(footer);
+
+    return card;
+}
+
+function createPeriodicTable() {
+    var container = document.createElement('div');
+    container.classList.add("elem-container");
+    document.body.appendChild(container);
+
+
+
+    doRow(row1);
+    doRow(row2);
+    doRow(row3);
+    doRow(row4);
+    doRow(row5);
+    doRow(row6);
+    doRow(row7);
+
+    doRow_LanthanideActinide(row_Lanthanide, "Lanthanide");
+    doRow_LanthanideActinide(row_Actinide, "Actinide");
+}
+
+// Helper function to create each row of the Periodic table.
+function doRow(rowNum) {
+    var n = 18;     // Number of columns.
+
+    var container = document.createElement('div');
+    container.classList.add("elem-container");
+    document.body.appendChild(container);
+
+    for (var i = 0; i < n; i++) {
+        var col = i % 3 + 1;
+
+        var c1;
+        if (!rowNum[i].hasOwnProperty('Symbol')) {
+            c1 = createCard("img-div-col0", rowNum[i].Number, rowNum[i].Symbol, rowNum[i].Name);
+            c1.style.visibility = 'hidden';
+        }
+
+        else if (rowNum[i].Symbol == '') {
+            c1 = createCard("img-div-col0", rowNum[i].Number, rowNum[i].Symbol, rowNum[i].Name);
+        }
+
+        else {
+            var divColour;
+            switch (rowNum[i].Type) {
+                case 'Metalloid': divColour = 'img-div-col1'; break;
+                case 'Alkali Metal': divColour = 'img-div-col2'; break;
+                case 'Alkaline Earth': divColour = 'img-div-col3'; break;
+                case 'Transition Metal': divColour = 'img-div-col4'; break;
+                case 'Basic Metal': divColour = 'img-div-col5'; break;
+                case 'Non-metal': divColour = 'img-div-col6'; break;
+                case 'Halogen': divColour = 'img-div-col7'; break;
+                case 'Noble Gas': divColour = 'img-div-col8'; break;
+                default: divColour = 'img-div-col9'; break;
+            }
+            c1 = createCard(divColour, rowNum[i].Number, rowNum[i].Symbol, rowNum[i].Name);
+        }
+
+        container.appendChild(c1);
+    }
+}
+
+// Helper function to display the Lanthanide and Actinide series of elements.
+function doRow_LanthanideActinide(rowNum) {
+    var n = 15;     // Number of columns.
+
+    var container = document.createElement('div');
+    container.classList.add("elem-container");
+    document.body.appendChild(container);
+
+    var typeName = rowNum[0].Type;
+    var divTypeName = document.createElement('div');
+    divTypeName.textContent = typeName + " Series";
+    divTypeName.classList.add("elem-container-La-Ac");
+    divTypeName.classList.add("title-La-Ac");
+    container.appendChild(divTypeName);
+
+    for (var i = 0; i < n; i++) {
+        var c1;
+        var divColour;
+        switch (rowNum[i].Type) {
+            case 'Lanthanide': divColour = 'img-div-col9'; break;
+            case 'Actinide': divColour = 'img-div-col10'; break;
+            default: divColour = 'img-div-col9'; break;
+        }
+        c1 = createCard(divColour, rowNum[i].Number, rowNum[i].Symbol, rowNum[i].Name);
+
+        container.appendChild(c1);
+    }
+}
+
+function createLegendBox() {
+    var legendBox = document.createElement('div');
+    legendBox.id = "legend";
+    legendBox.classList.add("legend-box");
+    document.body.appendChild(legendBox);
+
+    createLegendItem("Metalloid", "img-div-col1");
+    createLegendItem("Alkali Metal", "img-div-col2");
+    createLegendItem("Alkaline Earth", "img-div-col3");
+    createLegendItem("Transition Metal", "img-div-col4");
+    createLegendItem("Basic Metal", "img-div-col5");
+    createLegendItem("Non-metal", "img-div-col6");
+    createLegendItem("Halogen", "img-div-col7");
+    createLegendItem("Noble Gas", "img-div-col8");
+}
+
+function createLegendItem(elemType, itemClass) {
+    // Create the container for the colour and name.
+    var legendItem = document.createElement('div');
+    legendItem.classList.add("legend-item");
+
+    // Legend colour
+    var legendItemColour = document.createElement('div');
+    legendItemColour.classList.add("legend-item-col");
+    legendItemColour.classList.add(itemClass);
+
+    // Legend name
+    var legendItemName = document.createElement('div');
+    legendItemName.textContent = elemType;
+
+    // Add the colour and name to the container div.
+    legendItem.appendChild(legendItemColour);
+    legendItem.appendChild(legendItemName);
+
+    // Add the new item to the Legend box.
+    var legendBox = document.getElementById('legend');
+    legendBox.appendChild(legendItem);
+}
